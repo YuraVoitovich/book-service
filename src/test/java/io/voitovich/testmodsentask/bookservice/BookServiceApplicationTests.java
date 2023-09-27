a5438bbe6afd62cd40f7a1609785afefdea0e81c
voito <voitovich200233@gmail.com>
2023-09-27 20:13:27 +0300

    Disabled contextloads text

--- src/test/java/io/voitovich/testmodsentask/bookservice/BookServiceApplicationTests.java
+++ src/test/java/io/voitovich/testmodsentask/bookservice/BookServiceApplicationTests.java
@@ -1,12 +1,13 @@
 package io.voitovich.testmodsentask.bookservice;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BookServiceApplicationTests {
 
-    @Test
+    @Disabled
     void contextLoads() {
     }
 
