1a89fd694f63540ab3b99a0ded968bf7057005ba
voito <voitovich200233@gmail.com>
2023-09-30 06:08:42 +0300

    Exception handler added

---
+++ src/main/java/io/voitovich/testmodsentask/bookservice/exceptionhandler/RestControllerExceptionHandler.java
@@ -0,0 +1,19 @@
+package io.voitovich.testmodsentask.bookservice.exceptionhandler;
+
+
+import io.voitovich.testmodsentask.bookservice.exception.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestControllerExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(NotValidUUIDException.class)
+    public ResponseEntity<Object> handleNotValidUUIDException(NotValidUUIDException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+
+}
