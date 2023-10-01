c0b161927eea8746047d96eb5d04c803d02c41e4
voito <voitovich200233@gmail.com>
2023-10-01 23:00:41 +0300

    Add response http statuses

--- src/main/java/io/voitovich/testmodsentask/bookservice/controller/Controller.java
+++ src/main/java/io/voitovich/testmodsentask/bookservice/controller/Controller.java
@@ -3,6 +3,7 @@ package io.voitovich.testmodsentask.bookservice.controller;
 
 import io.voitovich.testmodsentask.bookservice.dto.BookDto;
 import io.voitovich.testmodsentask.bookservice.service.BookService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +22,7 @@ public class Controller {
     }
 
     @GetMapping("/books")
+    @ResponseStatus(HttpStatus.OK)
     @PreAuthorize("hasRole('modsen-user')")
     ResponseEntity<List<BookDto>> getAllBooks() {
         return ResponseEntity.ok(bookService.getAllBooks());
@@ -28,35 +30,41 @@ public class Controller {
 
 
     @GetMapping("/book/{id}")
+    @ResponseStatus(HttpStatus.OK)
     @PreAuthorize("hasRole('modsen-user')")
     ResponseEntity<BookDto> getBookById(@PathVariable(name = "id") String id) {
         return ResponseEntity.ok(bookService.getBookById(getUUIDFromString(id)));
     }
 
     @GetMapping("/book-isbn/{isbn}")
+    @ResponseStatus(HttpStatus.OK)
     @PreAuthorize("hasRole('modsen-user')")
     ResponseEntity<BookDto> getBookByIsbn(@PathVariable(name = "isbn") String isbn) {
         return ResponseEntity.ok(bookService.getBookByISBN(isbn));
     }
 
     @PostMapping("book/{id}")
+    @ResponseStatus(HttpStatus.OK)
     @PreAuthorize("hasRole('modsen-user')")
     void takeBook(@PathVariable(name = "id") String id) {
         bookService.takeBook(getUUIDFromString(id));
     }
     @PostMapping("/book")
+    @ResponseStatus(HttpStatus.OK)
     @PreAuthorize("hasRole('modsen-admin')")
     void updateBook(@RequestBody BookDto bookDto) {
         bookService.updateBook(bookDto);
     }
 
     @PutMapping("/book")
+    @ResponseStatus(HttpStatus.CREATED)
     @PreAuthorize("hasRole('modsen-admin')")
     void addBook(@RequestBody BookDto bookDto) {
         bookService.addBook(bookDto);
     }
 
     @DeleteMapping("/book/{id}")
+    @ResponseStatus(HttpStatus.OK)
     @PreAuthorize("hasRole('modsen-admin')")
     void deleteBook(@PathVariable(name = "id") String id) {
         bookService.deleteBook(getUUIDFromString(id));
